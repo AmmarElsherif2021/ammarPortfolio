5b5d91fa274bfa66d227f87bffbee79d8c59f7d1
Ammar <ammar.alshareef2021@outlook.com>
2024-03-20 05:19:19 +0200

    .

--- src/components/Strip/Strip.jsx
+++ src/components/Strip/Strip.jsx
@@ -1,16 +1,38 @@
 import './Strip.css'
 import Bubble from '../Bubble/Bubble'
-const Strip=(props)=>{
-    return(
-        <div className='strip'>
-           { props.libsList.map((x)=>(
-                <div><Bubble img={x.img} stat={x.stat}/></div>
-            ))}
-            </div>
-         
-            
+import { useState, useEffect } from 'react';
+const Strip = (props) => {
+    const [scrollPosition, setScrollPosition] = useState(0);
 
-        
+    const scrollLeft = () => {
+        setScrollPosition(scrollPosition - 300);
+    };
+
+    const scrollRight = () => {
+        setScrollPosition(scrollPosition + 300);
+    };
+
+    useEffect(() => {
+        const parent = document.querySelector('.strip-rail');
+        if (parent) {
+            parent.scrollLeft = scrollPosition;
+        }
+    }, [scrollPosition]);
+    return (
+        <div className='strip'>
+            <button onClick={scrollLeft}>{'<'}</button>
+            <div className='strip-rail'>
+                {props.libsList.map((x) => (
+                    <div><Bubble img={x.img} stat={x.stat} /></div>
+
+                ))}
+            </div>
+            <button onClick={scrollRight}>{'>'}</button>
+        </div>
+
+
+
+
     )
 }
 export default Strip
