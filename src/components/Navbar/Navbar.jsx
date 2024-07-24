60e7d494ea08f80f14df64abb282110f8beeb6a2
ammar <ammar.alshareef2014@gmail.com>
2024-07-25 00:44:25 +0200

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,22 @@
-import { userContext, UserProvider } from '../../routes/userContext';
-import { useState, useContext, useEffect } from 'react';
-import './Navbar.css'
-import useWindowDimensions from '../../routes/windowContext';
+import { userContext, UserProvider } from "../../routes/userContext";
+import { useState, useContext, useEffect } from "react";
+import "./Navbar.css";
+import useWindowDimensions from "../../routes/windowContext";
 import { Outlet, Link } from "react-router-dom";
-import Logo from '../../../src/assets/Avatar.svg';
-import toggleLogo from '../../../src/assets/menu.svg'
-
-
-
+import Logo from "../../../src/assets/Avatar.svg";
+import toggleLogo from "../../../src/assets/menu.svg";
 
 const Login = () => {
   const { logged, setLogged } = useContext(userContext);
-  const [userData, setUserData] = useState({ name: '', password: '' });
-
+  const [userData, setUserData] = useState({ name: "", password: "" });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (userData.name === 'ammar' && userData.password === '4331077') {
+    if (userData.name === "ammar" && userData.password === "4331077") {
       setLogged(true);
-      console.log('You are logged in');
+      console.log("You are logged in");
     } else {
-      console.log('Error');
+      console.log("Error");
     }
   };
 
@@ -33,20 +29,21 @@ const Login = () => {
   }, [userContext]);
 
   return (
-
-
-
     <div className="login-container">
       {logged ? (
-
-        <div className='login' > <Link to={`Admin`}><h3>Dashboard</h3></Link><Outlet /></div>
-
+        <div className="login">
+          {" "}
+          <Link to={`Admin`}>
+            <h3>Dashboard</h3>
+          </Link>
+          <Outlet />
+        </div>
       ) : (
-        <form className='login' onSubmit={handleSubmit}>
+        <form className="login" onSubmit={handleSubmit}>
           <input
             type="text"
             id="name"
-            placeholder='name'
+            placeholder="name"
             value={userData.name}
             onChange={(e) =>
               setUserData((prev) => ({ ...prev, name: e.target.value }))
@@ -55,14 +52,15 @@ const Login = () => {
           <input
             type="password"
             id="password"
-            placeholder='password'
+            placeholder="password"
             value={userData.password}
             onChange={(e) =>
               setUserData((prev) => ({ ...prev, password: e.target.value }))
             }
           />
-          <button className='login-btn' type="submit">Login</button>
-
+          <button className="login-btn" type="submit">
+            Login
+          </button>
         </form>
       )}
     </div>
@@ -75,16 +73,22 @@ const Navbar = () => {
   const [toggled, setToggeled] = useState(false);
   return (
     <UserProvider>
-      <div className={width <= 900 && toggled ? 'nav-v' : 'nav'}>
-        <div >
-
-          <Link to={`/`}><img className='avatar' src={Logo} /></Link>
+      <div className={width <= 900 && toggled ? "nav-v" : "nav"}>
+        <div>
+          <Link to={`/`}>
+            <img className="avatar" src={Logo} />
+          </Link>
           <Outlet />
         </div>
-        {width <= 900 ? <div className='toggle' onClick={() => setToggeled((prev) => !prev)}><img className='menu-img' src={toggleLogo} /></div> : <div></div>}
-        {(width <= 900 && toggled) || width > 900 ?
-
-          <div className='menu' >
+        {width <= 900 ? (
+          <div className="toggle" onClick={() => setToggeled((prev) => !prev)}>
+            <img className="menu-img" src={toggleLogo} />
+          </div>
+        ) : (
+          <div></div>
+        )}
+        {(width <= 900 && toggled) || width > 900 ? (
+          <div className="menu">
             <div>
               <h4>Browse</h4>
             </div>
@@ -95,15 +99,12 @@ const Navbar = () => {
               <h4>Contact</h4>
             </div>
             <Login />
-
-          </div> : <div></div>}
-
-
-
-
+          </div>
+        ) : (
+          <div></div>
+        )}
       </div>
     </UserProvider>
-  )
-
-}
-export default Navbar
+  );
+};
+export default Navbar;
